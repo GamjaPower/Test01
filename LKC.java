369f493dbde559ee74897654e483e5d74ea5ee7e
GamjaPower <sst9696@gmail.com>
2020-12-03 10:13:46 +0900

    Test

---
+++ LKC.java
@@ -0,0 +1,15 @@
+/**
+ * @author jason96
+ *
+ */
+public class LKC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("LKC");
+	}
+
+}
